190f3553f27f82db02bbd3e311105a451659f432
Trung Dinh Quang <trungdq88@gmail.com>
2016-04-23 00:13:26 +0700

    Fix path

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts('/cache-polyfill.js');
+importScripts('cache-polyfill.js');
 
 self.addEventListener('install', function(e) {
   e.waitUntil(
@@ -6,8 +6,6 @@ self.addEventListener('install', function(e) {
       return cache.addAll([
         '/',
         '/index.html',
-        '/index.html?homescreen=1',
-        '/?homescreen=1',
         '/main.css',
         '/main.js',
       ]).then(function() {
